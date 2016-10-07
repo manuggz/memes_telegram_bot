c12b7536aac9e333816c7c7dc440fe3dad5d59fb
manuggz <manuelggonzalezm@gmail.com>
2016-10-07 18:35:39 -0400

    Fixed bugs

--- BotTelegram/tests.py
+++ BotTelegram/tests.py
@@ -24,79 +24,79 @@ class TestBot(TestCase):
                         {u'text': "",u'from': cls.user_from,u'chat': cls.chat,u'message_id': 905475,
                          u'date': 1475391962}, u'update_id': 25256647,u'debug':True}
 
-    # def test_start(self):
-    #     self.consulta[u'message'][u'text'] = u"/start"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta),content_type="text/json",secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #
-    # def test_help(self):
-    #     self.consulta[u'message'][u'text'] = u"/help"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta),content_type="text/json", secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #
-    # def test_help_sendme(self):
-    #     self.consulta[u'message'][u'text'] = u"/help sendme"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta), content_type="text/json", secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_help_create(self):
-    #     self.consulta[u'message'][u'text'] = u"/help create"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta), content_type="text/json", secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_help_random(self):
-    #     self.consulta[u'message'][u'text'] = u"/help random"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta), content_type="text/json", secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #
-    # def test_sendme_sin_comandos(self):
-    #     self.consulta[u'message'][u'text'] = u"/sendme"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta), content_type="text/json", secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_random(self):
-    #
-    #     self.consulta[u'message'][u'text'] = u"forever alone"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta), content_type="text/json", secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #     self.consulta[u'message'][u'text'] = u"/random"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta), content_type="text/json", secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
-    #
-    #     self.consulta[u'message'][u'text'] = u"/another"
-    #     response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
-    #                                 json.dumps(self.consulta), content_type="text/json", secure=True)
-    #
-    #     # Check that the response is 200 OK.
-    #     self.assertEqual(response.status_code, 200)
+    def test_start(self):
+        self.consulta[u'message'][u'text'] = u"/start"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta),content_type="text/json",secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_help(self):
+        self.consulta[u'message'][u'text'] = u"/help"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta),content_type="text/json", secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_help_sendme(self):
+        self.consulta[u'message'][u'text'] = u"/help sendme"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta), content_type="text/json", secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+    def test_help_create(self):
+        self.consulta[u'message'][u'text'] = u"/help create"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta), content_type="text/json", secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+    def test_help_random(self):
+        self.consulta[u'message'][u'text'] = u"/help random"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta), content_type="text/json", secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_sendme_sin_comandos(self):
+        self.consulta[u'message'][u'text'] = u"/sendme"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta), content_type="text/json", secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+    def test_random(self):
+
+        self.consulta[u'message'][u'text'] = u"forever alone"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta), content_type="text/json", secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        self.consulta[u'message'][u'text'] = u"/random"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta), content_type="text/json", secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        self.consulta[u'message'][u'text'] = u"/another"
+        response = self.client.post('/BotTelegram/119646075:AAFsQGgw8IaLwvRZX-IBO9mgV3k048NpuMg/',
+                                    json.dumps(self.consulta), content_type="text/json", secure=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
 
     def test_boton_random(self):
         self.consulta[u'message'][u'text'] = u"forever alone"
